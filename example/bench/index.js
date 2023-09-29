542586594af62b657a06c859c66525aa14409365
Guilherme Nascimento <brcontainer@yahoo.com.br>
2023-09-29 11:02:29 -0300

    remove unused

--- example/bench/index.js
+++
@@ -1,5 +0,0 @@
-const { Teeny } = require('../../src/Teeny.js');
-
-const app = new Teeny(`${__dirname}/routes.js`, 7000);
-
-app.exec();
--- example/bench/routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-    // Access http://localhost:7000/helloworld for see "Hello world"
-    app.action('GET', '/helloworld', (request, response) => {
-        return 'Hello World!';
-    });
-};
